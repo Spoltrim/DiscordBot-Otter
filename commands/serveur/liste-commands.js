49ff53344e85c1f91804d77f575ddeaf47b943b4
Spoltrim <cheronlucas1@gmail.com>
2024-11-22 23:44:15 +0100

    liste commande Fonctionnel

--- commands/serveur/liste-commands.js
+++ commands/serveur/liste-commands.js
@@ -1,14 +1,27 @@
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
-
-//faire la slash commande dans l'index ? a causes des dépendances circulaires
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("commandlist")
     .setDescription("Liste des commandes"),
   async execute(interaction) {
+    const commands = interaction.client.commands;
+    const commandList = commands
+      .map(
+        (cmd) =>
+          `**/${cmd.data.name}**: ${
+            cmd.data.description || "Pas de description."
+          }`
+      )
+      .join("\n");
+    const helpEmbed = new EmbedBuilder()
+      .setColor("#0099ff")
+      .setTitle("Liste des commandes disponibles")
+      .setDescription(commandList)
+      .setFooter({
+        text: "Utilisez une commande en tapant /<nom de la commande>",
+      });
     await interaction.reply({
-      embeds: [new EmbedBuilder().setDescription(``).setColor("#050033")],
+      embeds: [helpEmbed],
     });
   },
 };
